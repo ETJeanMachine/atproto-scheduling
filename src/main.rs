eb4f3d9e4489809c5b21a629dda67ac5d57acbb7
Eric Hamilton <ericjean873@gmail.com>
2025-06-11 19:18:52 -0700

    setup callback redirect correctly now

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
 mod utils;
 
 use axum::{Router, extract::Query, response::Redirect, routing::get};
-use std::collections::HashMap;
+use std::{clone, collections::HashMap};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(root))
+        .route("/client-metadata.json", get(client_metadata))
         .route("/oauth", get(oauth))
         .route("/oauth/callback", get(oauth_callback));
     let port = 8080;
     let bind = format!("0.0.0.0:{}", port);
     let listener = tokio::net::TcpListener::bind(bind).await.unwrap();
-    println!("Now running at http://localhost:{} !", port);
+    println!("Now running at http://full-gobbler-whole.ngrok-free.app !",);
+    println!("Test link: http://full-gobbler-whole.ngrok-free.app/oauth?handle=jeanmachine.dev",);
     println!(
-        "Test link: http://localhost:{}/oauth?handle=jeanmachine.dev",
-        port
+        "Test link: http://full-gobbler-whole.ngrok-free.app/oauth?handle=etjeanmachine.bsky.social",
     );
-    println!(
-        "Test link: http://localhost:{}/oauth?handle=etjeanmachine.bsky.social",
-        port
-    );
-    println!("Test link: http://localhost:{}/oauth?handle=nat.vg", port);
+    println!("Test link: http://full-gobbler-whole.ngrok-free.app/oauth?handle=nat.vg");
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -31,22 +28,28 @@ async fn root() -> &'static str {
 
 async fn oauth(Query(params): Query<HashMap<String, String>>) -> Redirect {
     let handle = params.get("handle").unwrap();
+    let client_id = "https://full-gobbler-whole.ngrok-free.app/client-metadata.json";
     let oauth_url = match utils::fetch_pds(handle.clone()).await {
-        Ok(pds_endpoint) => format!("{}/oauth", pds_endpoint),
+        Ok(pds_endpoint) => format!(
+            "{}/oauth/authorize?client_id={}&redirect_uri={}&response_type=code&scope=atproto",
+            pds_endpoint, client_id, "https://full-gobbler-whole.ngrok-free.app/oauth/callback"
+        ),
         Err(_) => return Redirect::to("/"),
     };
-    let client = reqwest::Client::new();
-    let client_id_url = format!("{}/client", oauth_url);
-    let client_id = client
-        .post(client_id_url)
-        .query(&[("name", "atproto-scheduling")])
-        .send()
-        .await
-        .unwrap();
-    println!("{:?}", client_id);
     Redirect::to(&oauth_url)
 }
 
 async fn oauth_callback() -> &'static str {
     "Callback executed!"
+}
+
+async fn client_metadata() -> axum::Json<serde_json::Value> {
+    axum::Json(serde_json::json!({
+        "client_name": "atproto-scheduling",
+        "client_uri": "https://atproto-scheduling.com",
+        "redirect_uri": "https://atproto-scheduling.com/oauth/callback",
+        "response_types": ["code"],
+        "grant_types": ["authorization_code"],
+        "scope": "atproto"
+    }))
 }
